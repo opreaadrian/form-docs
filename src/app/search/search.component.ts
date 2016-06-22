6ac98a9ca0f4407ad67ea8b985df1722d210563d
Adrian Oprea <adrian@codesi.nz>
2016-06-22 21:05:03 +0300

    feat: Use Material Design card layout
    Signed-off-by: Adrian Oprea <adrian@codesi.nz>

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
+import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
+import { MdInput } from '@angular2-material/input';
 
 @Component({
   selector: 'search',
   template: `
-    <input type="search" placeholder="Contract, SRL ...">
-    <button md-raised-button>Find</button>
+    <md-card>
+          <md-card-title>Docomente</md-card-title>
+          <md-card-subtitle>Found one you need?</md-card-subtitle>
+       <md-card-content>
+         <md-input placeholder="Contract, SRL ..."></md-input>
+         <button md-raised-button>Find</button>
+       </md-card-content>
+    </md-card>
   `,
-  directives: [ MD_BUTTON_DIRECTIVES ]
+  directives: [ MD_CARD_DIRECTIVES, MD_BUTTON_DIRECTIVES, MdInput ]
 })
 
 export class Search {}
