3c8a40f103efc3037d10595508e06e42861400ec
cristinel.pirvulescu <pirvulescu.cristi@gmail.com>
2016-06-26 12:45:17 +0300

    feat: Add angular2-material modules
    Signed-off-by: cristinel.pirvulescu <pirvulescu.cristi@gmail.com>
    
    feat: Add html in a separate template file
    
    Signed-off-by: cristinel.pirvulescu <pirvulescu.cristi@gmail.com>
    
    feat(search.component.ts): Update url to use new template
    
    - update url to use the new template file (relative path);
    - import toolbar module;
    - add toolbar directives;
    
    Signed-off-by: cristinel.pirvulescu <pirvulescu.cristi@gmail.com>
    
    feat: Add separate css file
    
    Signed-off-by: cristinel.pirvulescu <pirvulescu.cristi@gmail.com>
    
    chore(search.component.html): Update content
    
    Signed-off-by: cristinel.pirvulescu <pirvulescu.cristi@gmail.com>

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,21 +1,20 @@
 import { Component } from '@angular/core';
 import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
+import {MD_TOOLBAR_DIRECTIVES} from '@angular2-material/toolbar';
 import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
 import { MdInput } from '@angular2-material/input';
 
 @Component({
+  moduleId: module.id,
   selector: 'search',
-  template: `
-    <md-card>
-          <md-card-title>Docomente</md-card-title>
-          <md-card-subtitle>Found one you need?</md-card-subtitle>
-       <md-card-content>
-         <md-input placeholder="Contract, SRL ..."></md-input>
-         <button md-raised-button>Find</button>
-       </md-card-content>
-    </md-card>
-  `,
-  directives: [ MD_CARD_DIRECTIVES, MD_BUTTON_DIRECTIVES, MdInput ]
+  templateUrl: 'search.component.html',
+  styleUrls: ['search.component.css'],
+  directives: [
+    MD_CARD_DIRECTIVES,
+    MD_TOOLBAR_DIRECTIVES,
+    MD_BUTTON_DIRECTIVES,
+    MdInput
+  ]
 })
 
 export class Search {}
