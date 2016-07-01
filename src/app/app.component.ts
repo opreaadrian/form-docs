1140cd8c7f62b17140f7a48981cbec4e111af5c8
cristinel.pirvulescu <pirvulescu.cristi@gmail.com>
2016-07-01 08:55:50 +0300

    feat(app.component.ts): Add [Search] provider
    Signed-off-by: cristinel.pirvulescu <pirvulescu.cristi@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { Search } from './search/search.component';
+
 @Component({
   selector: 'my-app',
+  providers: [Search],
   template: '<search></search>',
   directives: [Search]
 })
