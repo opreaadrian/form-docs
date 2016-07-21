e2cd5ea7398514f046e70087e5ef900b92bdffc6
cristinel.pirvulescu <pirvulescu.cristi@gmail.com>
2016-07-21 20:06:45 +0300

    refactor(main-app): Change main application component name

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { bootstrap }    from '@angular/platform-browser-dynamic';
 import { JSONP_PROVIDERS } from '@angular/http';
-import { AppComponent } from './app/app.component';
+import { FormDocApp } from './app/app.component';
 
 bootstrap(AppComponent, [JSONP_PROVIDERS]).catch(err => console.error(err));
