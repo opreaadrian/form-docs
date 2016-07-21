398e1ed286bd132ac4c0a88f1d05824087a56276
cristinel.pirvulescu <pirvulescu.cristi@gmail.com>
2016-07-21 20:29:23 +0300

    feat(app.component): Add routing functionality
    - add `Footer` component, as it is required
    for navigation;
    - add `CategoriesList` component, as it is required
    for navigation;
    - add `<router-outlet>`, where the routed component
    will be displayed;
    - `precompile` the newly added components, so that the
    browser doesn't waste time compiling;
    - update `directives` with the newly added components;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
+
+import { CategoriesList } from './categories/categories.component';
+import { Footer } from './footer/footer.component';
 import { Search } from './search/search.component';
 
 @Component({
-  selector: 'form-doc',
-  providers: [Search],
-  template: '<search></search>',
-  directives: [Search]
+  selector: 'form-doc-app',
+  providers: [
+    Search,
+    Footer
+  ],
+  template: `
+    <router-outlet></router-outlet>
+    `,
+  directives: [
+    Search,
+    Footer,
+    ROUTER_DIRECTIVES
+  ],
+  precompile: [FormDocApp, CategoriesList, Search]
 })
-export class AppComponent { }
+
+export class FormDocApp {}
