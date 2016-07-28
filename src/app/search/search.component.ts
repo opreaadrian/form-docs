cfc43b1de5190ee89931f44314568ab6472266d2
cristinel.pirvulescu <pirvulescu.cristi@gmail.com>
2016-07-28 23:11:48 +0300

    feat(search): Add new forms functionality

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
-import { Injectable } from '@angular/core';
-import { NgForm } from '@angular/common';
+import { Injectable, ViewChild } from '@angular/core';
+import { FORM_DIRECTIVES, FORM_PROVIDERS, NgForm, FormBuilder, FormGroup, FormControl, REACTIVE_FORM_DIRECTIVES, Validators } from '@angular/forms';
 import { URLSearchParams, Jsonp } from '@angular/http';
 import { Control } from '@angular/common';
 import { Observable } from 'rxjs/Observable';
+import { CORE_DIRECTIVES } from '@angular/common';
+
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/operator/switchMap';
+
 import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
 import { MD_TOOLBAR_DIRECTIVES } from '@angular2-material/toolbar';
 import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
@@ -18,11 +21,14 @@ import { SearchService } from './service.component';
 
 @Component({
   moduleId: module.id,
-  providers: [SearchService],
+  providers: [SearchService, FORM_PROVIDERS],
   selector: 'search',
   templateUrl: 'search.component.html',
   styleUrls: ['search.component.css'],
   directives: [
+    CORE_DIRECTIVES,
+    FORM_DIRECTIVES,
+    REACTIVE_FORM_DIRECTIVES,
     MD_CARD_DIRECTIVES,
     MD_TOOLBAR_DIRECTIVES,
     MD_BUTTON_DIRECTIVES,
@@ -34,11 +40,20 @@ import { SearchService } from './service.component';
 
 export class Search {
   items: Observable<Array<string>>;
-  term = new Control();
-  constructor(private searchService: SearchService) {
-    this.items = this.term.valueChanges
-                 .debounceTime(350)
-                 .distinctUntilChanged()
-                 .switchMap(term => this.searchService.search(term));
+  searchForm: FormGroup;
+  events: any[] = [];
+  //term = new Control('');
+
+  constructor(private searchService: SearchService, formBuilder: FormBuilder) {
+    this.searchForm = formBuilder.group({
+        term: ['', [<any>Validators.required, <any>Validators.minLength(5)]]
+    });
+
+    // this.searchForm.valueChanges
+    //   .map((value) => {console.log('value: ', value);})
+                //  .debounceTime(350)
+                //  .distinctUntilChanged()
+                //  .switchMap(term => this.searchService.search(term))
+                //  .subscribe((result) => {console.log('result: ', result);});
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { disableDeprecatedForms, provideForms } from '@angular/forms';
 import { JSONP_PROVIDERS } from '@angular/http';
+import { CORE_DIRECTIVES, FORM_DIRECTIVES } from '@angular/common';
 import { FormDocApp } from './app/app.component';
 import { appRouterProviders } from './app/app.routes';
 
 bootstrap(FormDocApp, [
   appRouterProviders,
+  disableDeprecatedForms(),
+  provideForms(),
+  CORE_DIRECTIVES,
+  FORM_DIRECTIVES,
   JSONP_PROVIDERS
 ]).catch(error => console.error(error));
