1dfdddfbc42494e94fafcf90c27991817d05ee76
cristinel.pirvulescu <pirvulescu.cristi@gmail.com>
2016-08-09 09:26:27 +0300

    feat(app.component): Add provider for `SearchService` in the main app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,13 @@ import { ROUTER_DIRECTIVES } from '@angular/router';
 import { CategoriesList } from './categories/categories.component';
 import { Footer } from './footer/footer.component';
 import { Search } from './search/search.component';
+import { SearchService } from './search/service.component';
 
 @Component({
   selector: 'form-doc-app',
   providers: [
     Search,
+    SearchService,
     Footer
   ],
   template: `
